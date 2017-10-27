5095450ed1bce927bd356aa9b0b43d58c4c1389c
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-10-27 13:25:46 +0100

    define schema

---
+++ app/data/schema.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const { makeExecutableSchema } = require('graphql-tools')
+const resolvers = require('./resolvers')
+
+// Define our schema using the GraphQL schema language
+const typeDefs = `
+  type User {
+    id: Int!
+    username: String!
+    email: String!
+    posts: [Post]
+  }
+
+  type Post {
+    id: Int!
+    title: String!
+    slug: String!
+    content: String!
+    user: User!
+  }
+
+  type Query {
+    allUsers: [User]
+    fetchUser(id: Int!): User
+    allPosts: [Post]
+    fetchPost(id: Int!): Post
+  }
+
+  type Mutation {
+    login (email: String!, password: String!): String
+    createUser (username: String!, email: String!, password: String!): User
+    addPost (title: String!, content: String!): Post
+  }
+`
+
+module.exports = makeExecutableSchema({ typeDefs, resolvers })
